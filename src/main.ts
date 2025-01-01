d870654a425e3d7a1e1b0f98ecfd75702b4f3318
Quoc Hung <51414190+qhung312@users.noreply.github.com>
2025-01-01 22:39:54 +0700

    chore: initial repo setup (#1)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+
+bootstrap()
+  .then(() => console.log('Application is running on: http://localhost:3000'))
+  .catch(error => console.error(error));
