f7393c06f032d0602b72aa9b1234acdcd967ea80
Quoc Hung <truongquochung312@gmail.com>
2025-02-11 00:43:37 +0700

    test: error logging

--- src/videos/services/video-processing.service.ts
+++ src/videos/services/video-processing.service.ts
@@ -73,7 +73,7 @@ export class VideoProcessingService {
     return new Promise((resolve, reject) => {
       /* Chain error handling in case ffmpeg exits */
       ffmpegProc.on('error', (error: Error) => {
-        this.logger.error(`FFmpeg error: ${error.message}`);
+        this.logger.error(error);
         reject(error);
       });
       const hlsOutputStream = new VideoSegmentReadStream(
