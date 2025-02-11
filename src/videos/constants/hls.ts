377307775c1bfdaea8a439a827b6c935675830b3
Quoc Hung <truongquochung312@gmail.com>
2025-02-11 13:18:49 +0700

    feat: lower bitrate

--- src/videos/constants/hls.ts
+++ src/videos/constants/hls.ts
@@ -4,26 +4,26 @@
 export const HLS_RESOLUTIONS = [
   {
     height: 360,
-    fps30Bitrate: '800k',
-    fps60Bitrate: '1500k',
+    fps30Bitrate: '600k',
+    fps60Bitrate: '1000k',
     audioBitrate: '64k',
   },
   {
     height: 480,
-    fps30Bitrate: '2000k',
-    fps60Bitrate: '4000k',
+    fps30Bitrate: '1200k',
+    fps60Bitrate: '2000k',
     audioBitrate: '96k',
   },
   {
     height: 720,
-    fps30Bitrate: '4000k',
-    fps60Bitrate: '7500k',
+    fps30Bitrate: '2500k',
+    fps60Bitrate: '4500k',
     audioBitrate: '96k',
   },
   {
     height: 1080,
-    fps30Bitrate: '6000k',
-    fps60Bitrate: '12000k',
+    fps30Bitrate: '4000k',
+    fps60Bitrate: '6500k',
     audioBitrate: '160k',
   },
 ];
