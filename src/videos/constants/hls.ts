34f02fbd22762f00011c002709d38a9f77fd62bf
Quoc Hung <truongquochung312@gmail.com>
2025-02-11 17:08:43 +0700

    test: settings

--- src/videos/constants/hls.ts
+++ src/videos/constants/hls.ts
@@ -4,18 +4,26 @@
 export const HLS_RESOLUTIONS = [
   {
     height: 360,
+    maxRate: '1125k',
+    bufSize: '2250k',
     audioBitrate: '48k',
   },
   {
     height: 480,
+    maxRate: '1500k',
+    bufSize: '3000k',
     audioBitrate: '64k',
   },
   {
     height: 720,
+    maxRate: '3750k',
+    bufSize: '7500k',
     audioBitrate: '96k',
   },
   {
     height: 1080,
+    maxRate: '6750k',
+    bufSize: '13500k',
     audioBitrate: '128k',
   },
 ];
