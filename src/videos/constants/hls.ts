3d5f4eb4f231ff5a015400637b7b521cd4690fd4
Quoc Hung <truongquochung312@gmail.com>
2025-03-05 21:03:40 +0700

    feat(video): remove 1080 and 360 resolutions

--- src/videos/constants/hls.ts
+++ src/videos/constants/hls.ts
@@ -2,12 +2,12 @@
  * Ref: https://www.movavi.com/learning-portal/what-is-video-bitrate.html
  */
 export const HLS_RESOLUTIONS = [
-  {
-    height: 360,
-    maxRate: '1000k',
-    bufSize: '2000k',
-    audioBitrate: '64k',
-  },
+  // {
+  //   height: 360,
+  //   maxRate: '1000k',
+  //   bufSize: '2000k',
+  //   audioBitrate: '64k',
+  // },
   {
     height: 480,
     maxRate: '1500k',
@@ -20,10 +20,10 @@ export const HLS_RESOLUTIONS = [
     bufSize: '7000k',
     audioBitrate: '96k',
   },
-  {
-    height: 1080,
-    maxRate: '6000k',
-    bufSize: '12000k',
-    audioBitrate: '128k',
-  },
+  // {
+  //   height: 1080,
+  //   maxRate: '6000k',
+  //   bufSize: '12000k',
+  //   audioBitrate: '128k',
+  // },
 ];
